8d9f608a0371c80dec976e3b707bf73fa5113435
Tony Grimes <Tony.Grimes@sait.ca>
2019-10-29 09:14:09 -0600

    Added beginning of day in-class

---
+++ src/chapters/ch05/in-class/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const pageInfo = require('./pageInfo');
+
+const app = express();
+
+app.set('view engine','ejs'); // allows us to exclude the file extension
+
+app.get('/:page', function(request, response){
+  response.render(request.params.page,pageInfo[request.params.page]);
+})
+
+app.get('/', function(request, response){
+  response.render('index',pageInfo.index);
+})
+
+app.use(express.static(path.join(__dirname, 'assets')));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){
+  console.log(`Listening on port ${PORT}`);
+});
