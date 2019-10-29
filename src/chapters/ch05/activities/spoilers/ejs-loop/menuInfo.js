f1d95e97fbfc12c26a06cb25e341dbd0fb1fda05
Tony Grimes <acidtone@tonygrimes.com>
2019-10-29 10:06:06 -0600

    added spoiler menu object

---
+++ src/chapters/ch05/activities/spoilers/ejs-loop/menuInfo.js
@@ -0,0 +1,24 @@
+const menuInfo = [
+  {
+    label: 'Home',
+    link: 'index',
+    tite: 'Welcome!'
+  },
+  {
+    label: 'Resources',
+    link: 'resources',
+    tite: 'Links for Learning'
+  },
+  {
+    label: 'Glossary',
+    link: 'definitions',
+    tite: 'Terms and Definitions for this course'
+  },
+  {
+    label: 'Contact',
+    link: 'contact',
+    tite: 'Text is best.'
+  }  
+]
+
+module.exports = menuInfo;
