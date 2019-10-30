3da994864be2acc325ff6a125ecf1283a6f36b3b
Tony Grimes <acidtone@tonygrimes.com>
2019-10-30 12:33:12 -0600

    tweaked tool-time

--- src/chapters/ch06/tool-time/app.js
+++ src/chapters/ch06/tool-time/app.js
@@ -4,13 +4,13 @@
 const MongoClient = require('mongodb').MongoClient;
 
 // replace the uri string with your connection string.
-const uri = "mongodb+srv://<db-user>:<password>@cluster0-1c2nl.mongodb.net/test?retryWrites=true&w=majority"
-MongoClient.connect(uri, function(err, client) {
+const uri = "mongodb+srv://hagrid:aragogrip@cluster0-1c2nl.mongodb.net/test?retryWrites=true&w=majority"
+MongoClient.connect(uri,{ useUnifiedTopology: true }, function(err, client) {
    if(err) {
         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
    }
    console.log('Connected...');
-   const collection = client.db("sample_supplies").collection("devices");
+   const collection = client.db("test").collection("devices");
    // perform actions on the collection object
    client.close();
 });
