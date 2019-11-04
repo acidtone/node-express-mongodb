904f89ca011b94e89f0a7860b9fb6ec522c08322
Tony Grimes <acidtone@tonygrimes.com>
2019-11-04 05:32:55 -0700

    added (broken) mongoose get definitions

--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
-const definitions = require('./definitions');
+const Definition = require('./models/definitions');
 
 const app = express();
+mongoose.set('debug', true);
+
+mongoose.connect(process.env.DB_CONNECTION, { useUnifiedTopology: true,useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('DB Connected!!!');
+});
 
 app.set('view engine','ejs');
 
-app.get('/', function(request, response){
-  response.render('index',{definitions: definitions});
-})
+
+app.get('/', async function(request, response) {
+  try {
+
+    // let definitions = await Definition.find({});
+
+    const definitions = await Definition.find({});
+    response.render('index',{definitions: definitions});
+
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+// app.get('/', function(request, response){
+//   response.render('index',{definitions: definitions});
+// })
 
 app.use(express.static(path.join(__dirname, 'public')));
 
