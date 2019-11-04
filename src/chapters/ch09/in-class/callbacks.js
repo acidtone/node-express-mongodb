14d9e844fd1c6c887b99b51f4becea204446030f
Tony Grimes <Tony.Grimes@sait.ca>
2019-11-04 08:37:11 -0700

    added problem code

---
+++ src/chapters/ch09/in-class/callbacks.js
@@ -0,0 +1,37 @@
+const posts = [
+  {
+    title: "Post One",
+    postBody: "This is Post One"
+  },
+  {
+    title: "Post Two",
+    postBody: "This is Post Two"
+  }
+];
+
+const post = {
+    title: "Post Three",
+    postBody: "This is Post Three"
+  };
+
+function getPosts() {
+  setTimeout(function(){
+    let output = '';
+    posts.forEach(function(post,index){
+      output += `${post.title}\n`;
+    });
+
+    console.log(output);
+
+  }, 1000);
+}
+
+function createPost(post) {
+  setTimeout(function(){
+    posts.push(post);
+  }, 999); 
+}
+
+createPost(post);
+
+getPosts();
